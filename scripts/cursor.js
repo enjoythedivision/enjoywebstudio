536ecd3ef1cfd24137261eaf5b1a3b1b4047d736
Maria <147405522+enjoythedivision@users.noreply.github.com>
2025-06-20 14:12:58 +0300

    Update cursor.js

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -14,3 +14,19 @@ cards.forEach((card) => {
     cursor.classList.remove("hovering-project");
   });
 });
+
+
+
+
+
+gsap.registerPlugin(SplitText);
+gsap.set("h1", { opacity: 1 });
+
+let split = SplitText.create(".hero-title", { type: "chars" });
+//now animate each character into place from 20px below, fading in:
+gsap.from(split.chars, {
+  y: 20,
+  autoAlpha: 0,
+  stagger: 0.05
+});
+
