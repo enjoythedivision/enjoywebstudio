1d9409958aff1f82b9c2b16c901ea5c6e32b15ab
Maria <marialapps@gmail.com>
2025-10-16 19:07:31 +0300

    .

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,29 +1,7 @@
-const cursor = document.querySelector(".custom-cursor");
-const cards = document.querySelectorAll(".project-card");
-
-document.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.clientX + "px";
-  cursor.style.top = e.clientY + "px";
-});
-
-cards.forEach((card) => {
-  card.addEventListener("mouseenter", () => {
-    cursor.classList.add("hovering-project");
-  });
-  card.addEventListener("mouseleave", () => {
-    cursor.classList.remove("hovering-project");
-  });
-});
-
-
-
-
-
 gsap.registerPlugin(SplitText);
 gsap.set("h1", { opacity: 1 });
 
-let split = SplitText.create(".hero-title", { type: "chars" });
-//now animate each character into place from 20px below, fading in:
+let split = SplitText.create(".hero h1", { type: "chars" });
 gsap.from(split.chars, {
   y: 20,
   autoAlpha: 0,
